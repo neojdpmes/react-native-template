2471290160fc26e3cc3c0c24da0e5cfe264c555b
Daniel <neojdpm.es@gmail.com>
2022-07-18 22:09:29 +0200

    Added socket

--- App.tsx
+++ App.tsx
@@ -1,10 +1,126 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import * as MediaLibrary from 'expo-media-library';
+import * as FileSystem from 'expo-file-system';
+import { useEffect, useState } from 'react';
+import useSocket from './hooks/useSocket';
 
 export default function App() {
+  const [mediaPermission, requestPermission] = MediaLibrary.usePermissions();
+  const [allowCheckFiles, setAllowCheckFiles] = useState(false);
+  const [fileQueue, setFileQueue] = useState([] as any);
+  const [files, setFiles] = useState([] as any);
+  const { socket, isConnected } = useSocket();
+
+  useEffect(() => {
+    if (allowCheckFiles) checkfiles();
+  }, [allowCheckFiles])
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  useEffect(() => {
+    if (mediaPermission?.status === 'denied') {
+      Alert.alert('Permission required', 'Media permission required to check all files', [
+        { text: 'OK' },
+      ]);
+    }
+    if (mediaPermission?.granted) setAllowCheckFiles(true);
+  }, [mediaPermission]);
+
+  useEffect(() => {
+    if (isConnected) {
+      socket.on(`file-saved`, (data) => {
+        console.log(fileQueue.length);
+        console.log('File saved', data);
+        fileQueue.pop();
+        setFileQueue([...fileQueue]);
+      });
+    }
+    return () => {
+      socket.off('file-saved');
+    }
+  }, [isConnected, socket, fileQueue ])
+
+  useEffect(() => {
+    if (fileQueue.length > 0 && isConnected) {
+      const { image, album } = fileQueue[fileQueue.length - 1];
+      uploadFile(image, album);
+    }
+  }, [isConnected, fileQueue]);
+
+  async function checkfiles() {
+    /*
+    const albums = await MediaLibrary.getAlbumsAsync();
+    const files: any[] = [];
+    for (const album of albums) files.push.apply(files, await getAlbumFiles(album));
+    uploadFiles(files);
+    */
+    const files = await getAllFiles();
+    console.log(files);
+    console.log(files.length);
+    uploadFiles(files);
+  }
+  
+
+  const createAlbumFiles = (albumFiles: any[]) => {
+    const files: any = {};
+    for (const file of albumFiles) {
+      files[file.image.id] = file;
+      files[file.image.id].status = 'pending';
+    }
+    setFiles(files);
+  }
+
+  const uploadFiles = (albumFiles: any[]) => {
+    setFileQueue(albumFiles);
+    createAlbumFiles(albumFiles);
+  }
+
+  const uploadFile = async (image: MediaLibrary.Asset, album: MediaLibrary.Album) => {
+    try {
+      console.log('Uploading', image.filename);
+      const file = await FileSystem.readAsStringAsync(image.uri, { encoding: FileSystem.EncodingType.Base64 })
+      socket.emit('upload', { id: image.id, data: file, name: image.filename, album: album.title });
+      console.log('Emitted', image.filename);
+    } catch (err: any) {
+      console.log(err.message);
+      fileQueue.pop();
+      setFileQueue([...fileQueue]);
+    }
+  }
+
+  async function getAlbumFiles(album: MediaLibrary.Album) {
+    console.log(album.title, album.assetCount);
+    const files: any[] = [];
+    let hasNext = true;
+    let after;
+    while (hasNext) {
+      const images: MediaLibrary.PagedInfo<MediaLibrary.Asset> = await MediaLibrary.getAssetsAsync({ album: album.id, after });
+      hasNext = images.hasNextPage;
+      for (const image of images.assets) files.push({ image, album }); 
+      after = images.endCursor;
+    }
+    return files;
+  }
+
+  async function getAllFiles() {
+    const files: any[] = [];
+    let hasNext = true;
+    let after;
+    while (hasNext) {
+      const images: MediaLibrary.PagedInfo<MediaLibrary.Asset> = await MediaLibrary.getAssetsAsync({ after });
+      hasNext = images.hasNextPage;
+      for (const image of images.assets) files.push({ image }); 
+      after = images.endCursor;
+    }
+    return files;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Uploading files: {fileQueue.length} left</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ hooks/useSocket.tsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+
+const socket = io('ws://192.168.1.16:18092', {
+  transports: ['websocket']
+});
+
+export default function useSocket() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+
+  useEffect(() => {
+    console.log('socketing')
+    socket.on('connect', () => {
+      console.log('connected')
+      setIsConnected(true);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('disconnected')
+      setIsConnected(false);
+    });
+
+    return () => {
+      socket.off('connect');
+      socket.off('disconnect');
+    };
+  }, []);
+
+  return { socket, isConnected };
+}
